737ed551889c5f4f87b4267ddd9b42924691df5f
ishtiaq <khan98x@gmail.com>
2023-05-13 19:18:16 -0400

    updated search result

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -3,18 +3,14 @@ const router = express.Router();
 const ComputerItems = require('../models/computerStore'); 
 
 router.get('/search', async (req, res) => {
-    const query = req.query.q;
     try {
-      const items = await ComputerItems.find({ name: new RegExp(query, 'i') });
-      console.log(items);
-      res.render('searchResults', { products: items });
-
+        let query = req.query.q;
+        let searchResult = await ComputerItems.find({name: {$regex: query, $options: 'i'}});
+        res.render('searchResults', {products: searchResult});
     } catch (err) {
-      console.error(err);
-      res.status(500).send('An error occurred while searching for items');
+        console.error(err);
+        res.status(500).send('Server Error');
     }
 });
-
-  
 
 module.exports = router;
